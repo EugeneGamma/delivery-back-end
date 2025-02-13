577c7188e510988ebfa20c5772026f3af3f42835
EugeneGamma <eugene.gamma2003@gmail.com>
2025-02-13 06:27:39 +0500

    Added e2e tests

--- src/dishes/dishes.service.spec.ts
+++ src/dishes/dishes.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DishesService } from './dishes.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('DishesService', () => {
   let service: DishesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DishesService],
+      providers: [DishesService, PrismaService],
     }).compile();
 
     service = module.get<DishesService>(DishesService);
