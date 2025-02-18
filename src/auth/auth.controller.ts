5ee9b4450c1b702660fa3482ae08722efad05a97
EugeneGamma <eugene.gamma2003@gmail.com>
2025-02-18 23:52:54 +0500

    Added SWAGGER for documintationing API

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,16 +3,30 @@ import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
 
+@ApiTags("auth")
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
+    @ApiOperation({ summary: 'Регистрация нового пользователя' })
+    @ApiResponse({
+        status: 201,
+        description: 'Пользователь успешно зарегистрирован.',
+    })
+    @ApiBody({ type: RegisterDto })
     @Post('register')
     async register(@Body() dto: RegisterDto) {
         return this.authService.register(dto);
     }
 
+    @ApiOperation({ summary: 'Аутентификация пользователя' })
+    @ApiResponse({
+        status: 200,
+        description: 'Пользователь успешно аутентифицирован и получен JWT.',
+    })
+    @ApiBody({ type: LoginDto })
     @UseGuards(AuthGuard('local'))
     @Post('login')
     async login(@Body() dto: LoginDto) {
--- src/dishes/dto/create-dish.dto.ts
+++ src/dishes/dto/create-dish.dto.ts
@@ -1,27 +1,56 @@
-import { IsArray, IsBoolean, IsNumber, IsOptional, IsString } from 'class-validator';
+import { IsBoolean, IsNumber, IsOptional, IsString } from 'class-validator';
 import { Transform } from 'class-transformer';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class CreateDishDto {
+    @ApiProperty({
+        description: 'Название блюда',
+        example: 'Сунсал Чикен',
+    })
     @IsString()
     name: string;
 
+    @ApiProperty({
+        description: 'Описание блюда',
+        example: 'Куринные обжаренные кусочки филе',
+    })
     @IsString()
     description: string;
 
+    @ApiProperty({
+        description: 'Ингредиенты блюда в виде строки (разделённые запятыми)',
+        example: 'Помидоры, Макароны, Паста',
+    })
     @IsString()
-    ingredients: any; // Теперь это строка
+    ingredients: any;
 
+    @ApiProperty({
+        description: 'Цена блюда',
+        example: 50000,
+    })
     @IsNumber()
-    @Transform(({ value }) => parseFloat(value)) // Преобразуем строку в чис
+    @Transform(({ value }) => parseFloat(value))
     price: number;
 
+    @ApiProperty({
+        description: 'Категория блюда',
+        example: 'Вторые блюда',
+    })
     @IsString()
     category: string;
 
+    @ApiPropertyOptional({
+        description: 'Флаг доступности блюда',
+        example: true,
+    })
     @IsBoolean()
     @IsOptional()
     isAvailable?: boolean;
 
+    @ApiPropertyOptional({
+        description: 'URL изображения блюда',
+        example: '/uploads/dishes_(1).jpg',
+    })
     @IsString()
     @IsOptional()
     imageUrl?: string;
